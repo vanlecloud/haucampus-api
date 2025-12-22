535fb17481f6deeab28ac0c94cabcfcd862f863e
vanlecloud <hongvan6466@gmail.com>
2025-12-22 19:06:59 +0700

    Add root route

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const app = express();
 app.use(cors());
 
 app.use("/news", require("./routes/news.route"));
+app.get("/", (req, res) => {
+  res.send("HAUCampus API is running");
+});
 
 app.listen(PORT, () => {
   console.log("API running ON PORT", PORT);
