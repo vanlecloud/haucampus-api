8a5c86482a346981021068bb1ee26b5135740248
vanlecloud <hongvan6466@gmail.com>
2025-12-23 08:15:48 +0700

    dd

--- src/services/lichday.services.js
+++ src/services/lichday.services.js
@@ -1,37 +1,44 @@
 const axios = require("axios");
 
 async function getlichDay({ namHoc, hocKy, cookie }) {
-  const url = "https://tinchi.hau.edu.vn/GiangVien/LichGiangDay/LoadThoiKhoaBieu";
-
-  const res = await axios.post(
-    url,
-    new URLSearchParams({
-      namHoc,
-      hocKy,
-      dotHoc: 0
-    }).toString(),
-    {
-      headers: {
-        Cookie: cookie,
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "X-Requested-With": "XMLHttpRequest",
-        "User-Agent": "Mozilla/5.0"
+  try {
+    const res = await axios.post(
+      "https://tinchi.hau.edu.vn/GiangVien/LichGiangDay/LoadThoiKhoaBieu",
+      new URLSearchParams({
+        namHoc,
+        hocKy,
+        dotHoc: 0
+      }).toString(),
+      {
+        headers: {
+          Cookie: cookie,
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+          "X-Requested-With": "XMLHttpRequest",
+          "User-Agent": "Mozilla/5.0"
+        },
+        timeout: 10000
       }
+    );
+
+    if (!Array.isArray(res.data)) {
+      throw new Error("Cookie hết hạn hoặc chưa đăng nhập");
     }
-  );
 
-  const rawData = res.data;
+    return res.data.map(item => ({
+      maHocPhan: item.MaHocPhan,
+      tenHocPhan: item.TenHocPhan,
+      soTinChi: item.SoTinChi,
+      tenLopTinChi: item.TenLop,
+      phong: item.TenPhong,
+      tiet: item.TietHoc,
+      thu: `Thứ ${item.Thu}`,
+      ngayHoc: `${item.NgayBatDau} - ${item.NgayKetThuc}`
+    }));
 
-  return rawData.map(item => ({
-    maHocPhan: item.MaHocPhan,
-    tenHocPhan: item.TenHocPhan,
-    soTinChi: item.SoTinChi,
-    tenLopTinChi: item.TenLop,
-    phong: item.TenPhong,
-    tiet: item.TietHoc,
-    thu: `Thứ ${item.Thu}`,
-    ngayHoc: `${item.NgayBatDau} - ${item.NgayKetThuc}`
-  }));
+  } catch (err) {
+    console.error("❌ getlichDay error:", err.message);
+    throw err;
+  }
 }
 
 module.exports = { getlichDay };
