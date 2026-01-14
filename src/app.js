15bb2b73fe70cb6baa70b5d62e929bdc1014c226
a80project <hongvan6466@gmail.com>
2026-01-14 14:01:46 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,9 +11,11 @@ const newsRoute = require("./routes/news.route");
 app.use(cors());
 app.use(express.json());
 
+app.use("/daotao", require("./routes/daotao.route"));
 app.use("/news", newsRoute);
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.get("/", (req, res) => {
   res.send("HAUCampus API is running");
 });
